12d1b33664604395f7c0b534fc91d91aebb45f11
aj <aj@localhost>
2007-09-04 11:54:02 +0000

    Phoenix: - Implemented CPU instructions ADC through DAA - Made 8-bit register representations references instead of pointers. - Added some convenience flag defines.

--- cpu/cpu.cpp
+++ cpu/cpu.cpp
@@ -10,29 +10,350 @@ u16 CPU::hl;
 u16 CPU::sp;
 u16 CPU::pc;
 
-u8* CPU::a;
-u8* CPU::f;
-u8* CPU::b;
-u8* CPU::c;
-u8* CPU::d;
-u8* CPU::e;
-u8* CPU::h;
-u8* CPU::l;
+u8& CPU::a = *(((u8*)&af)+1);
+u8& CPU::f = *(((u8*)&af)+0);
+u8& CPU::b = *(((u8*)&bc)+1);
+u8& CPU::c = *(((u8*)&bc)+0);
+u8& CPU::d = *(((u8*)&de)+1);
+u8& CPU::e = *(((u8*)&de)+0);
+u8& CPU::h = *(((u8*)&hl)+1);
+u8& CPU::l = *(((u8*)&hl)+0);
 
 
 void CPU::Initialize()
 {
-	a=((u8*)(&af))+1;
-	f=((u8*)(&af))+0;
-	b=((u8*)(&bc))+1;
-	c=((u8*)(&bc))+0;
-	d=((u8*)(&de))+1;
-	e=((u8*)(&de))+0;
-	h=((u8*)(&hl))+1;
-	l=((u8*)(&hl))+0;
+	
 }
 
 void CPU::Reset()
 {
 	Initialize();
+}
+
+
+
+
+//////////////////////////////////////////////////////////////////
+
+
+void CPU::ExecADC(u8* target, u8 value)
+{
+	int res = *target + value + TST_C;
+	
+	//Z
+	if((res & 0xff) == 0) 
+		SET_Z;
+	else 
+		RES_Z;
+
+	//N
+	RES_N;
+
+	//H
+	if( (res & 0x10) && !(*target & 0x10) )
+		SET_H;
+	else
+		RES_H;
+
+	//C
+	if(res & 0x100) 
+		SET_C;
+	else 
+		RES_C;
+
+	*target = (u8)res;
+}
+
+void CPU::ExecADC(u16* target, u16 value)
+{
+	int res = *target + value + TST_C;
+
+	//Z
+	if((res & 0xffff) == 0) 
+		SET_Z;
+	else 
+		RES_Z;
+
+	//N
+	RES_N;
+
+	//H unaffected
+
+	//C
+	if(res & 0x10000) 
+		SET_C;
+	else 
+		RES_C;
+
+	*target = (u16)res;
+}
+
+void CPU::ExecADD(u8* target, u8 value)
+{
+	int res = *target + value;
+	
+	//Z
+	if((res & 0xff) == 0) 
+		SET_Z;
+	else 
+		RES_Z;
+
+	//N
+	RES_N;
+
+	//H
+	if( (res & 0x10) && !(*target & 0x10) )
+		SET_H;
+	else
+		RES_H;
+
+	//C
+	if(res & 0x100) 
+		SET_C;
+	else 
+		RES_C;
+
+	*target = (u8)res;
+}
+
+void CPU::ExecADD(u16* target, u16 value)
+{
+	int res = *target + value;
+
+	//Z
+	if((res & 0xffff) == 0) 
+		SET_Z;
+	else 
+		RES_Z;
+
+	//N
+	RES_N;
+
+	//H unaffected
+
+	//C
+	if(res & 0x10000) 
+		SET_C;
+	else 
+		RES_C;
+
+	*target = (u16)res;
+}
+
+void CPU::ExecAND(u8 value)
+{
+	a = a & value;
+
+	//Z
+	if(a == 0)
+		SET_Z;
+	else
+		RES_Z;
+
+	//N
+	RES_N;
+
+	//H
+	SET_H;
+
+	//C
+	RES_C;
+}
+
+void CPU::ExecBIT(u8 value, int n)
+{
+	//Z
+	if(value & (1<<n))
+		SET_Z;
+	else
+		RES_Z;
+
+	//N
+	RES_N;
+
+	//H
+	SET_H;
+
+	//C unaffected
+}
+
+void CPU::ExecCALL(bool test, u16 address)	//TODO
+{
+	if(test)
+		pc = address;
+}
+
+void CPU::ExecCCF()
+{
+	//Z unaffected
+
+	//N
+	RES_N;
+
+	//H
+	if(TST_C)
+		SET_H;
+	else
+		RES_H;
+	
+	//C
+	INV_C;	
+}
+
+void CPU::ExecCP(u8 value)
+{
+	int res = a - value;
+
+	//Z
+	if(res == 0)
+		SET_Z;
+	else
+		RES_Z;
+
+	//N
+	SET_N;
+
+	//H
+	//???
+	if( !(res & 0x10) && (value & 0x10) )
+		SET_H;
+	else
+		RES_H;
+
+	//C
+	//???
+	if(res < 0)
+		SET_C;
+	else
+		RES_C;
+}
+
+void CPU::ExecCPL()
+{
+	a ^= 0xff;
+
+	//Z unaffected
+
+	//N
+	SET_N;
+
+	//H
+	SET_H;
+
+	//C unaffected
+}
+
+void CPU::ExecDAA()
+{
+	if(TST_N)
+	{
+		if(TST_C)
+		{
+			if(TST_H)
+			{
+				a += 0x9a;
+				SET_C;
+			}
+			else
+			{
+				a += 0xa0;
+				SET_C;
+			}
+		}
+		else
+		{
+			if(TST_H)
+			{
+				a += 0xfa;
+				RES_C;
+			}
+			else
+			{
+				RES_C;
+			}
+		}
+	}
+	else
+	{
+		if(TST_C)
+		{
+			if(TST_H)
+			{
+				a += 0x66;
+				SET_C;
+			}
+			else
+			{
+				if((a & 0x0f) >= 0x0a)
+				{
+					a += 0x66;
+					SET_C;
+				}
+				else
+				{
+					a += 0x60;
+					SET_C;
+				}
+			}
+		}
+		else
+		{
+			if(TST_H)
+			{
+				if((a & 0xf0) >= 0xa0)
+				{
+					a += 0x66;
+					SET_C;
+				}
+				else
+				{
+					a += 0x06;
+					RES_C;
+				}
+			}
+			else
+			{
+				if((a & 0x0f) >= 0x0a)
+				{
+					if((a & 0xf0) >= 0x90)
+					{
+						a += 0x66;
+						SET_C;
+					}
+					else
+					{
+						a += 0x06;
+						RES_C;
+					}
+				}
+				else
+				{
+					if((a & 0xf0) >= 0x0a)
+					{
+						a += 0x60;
+						SET_C;
+					}
+					else
+					{
+						RES_C;
+					}
+				}
+			}
+		}
+	}
+
+
+	//Z
+	if(a == 0)
+		SET_Z;
+	else
+		RES_Z;
+
+	//N unaffected
+
+	//H
+	//???
+	//unaffected?
+
+	//C set above
 }
--- cpu/cpu.h
+++ cpu/cpu.h
@@ -4,47 +4,77 @@
 #include "../common/types.h"
 
 
-#define SET_Z (*f |= 0x80)
-#define SET_N (*f |= 0x40)
-#define SET_H (*f |= 0x20)
-#define SET_C (*f |= 0x10)
+//Flag positions
+#define BIT_Z (7)
+#define BIT_N (6)
+#define BIT_H (5)
+#define BIT_C (4)
 
-#define RES_Z (*f &= ~0x80)
-#define RES_N (*f &= ~0x40)
-#define RES_H (*f &= ~0x20)
-#define RES_C (*f &= ~0x10)
+//Set flag
+#define SET_Z (f |= 0x80)
+#define SET_N (f |= 0x40)
+#define SET_H (f |= 0x20)
+#define SET_C (f |= 0x10)
 
-#define TST_Z (*f & 0x80)
-#define TST_N (*f & 0x40)
-#define TST_H (*f & 0x20)
-#define TST_C (*f & 0x10)
+//Reset flag
+#define RES_Z (f &= ~0x80)
+#define RES_N (f &= ~0x40)
+#define RES_H (f &= ~0x20)
+#define RES_C (f &= ~0x10)
+
+//Return flag value if set, 0 otherwise
+#define VAL_Z ((u8)(f & 0x80))
+#define VAL_N ((u8)(f & 0x40))
+#define VAL_H ((u8)(f & 0x20))
+#define VAL_C ((u8)(f & 0x10))
+
+//Return 1 if flag is set, 0 otherwise
+#define TST_Z (u8)(VAL_Z >> BIT_Z)
+#define TST_N (u8)(VAL_N >> BIT_N)
+#define TST_H (u8)(VAL_H >> BIT_H)
+#define TST_C (u8)(VAL_C >> BIT_C)
+
+//Invert flag
+#define INV_Z (f ^= (1<<BIT_Z))
+#define INV_N (f ^= (1<<BIT_N))
+#define INV_H (f ^= (1<<BIT_H))
+#define INV_C (f ^= (1<<BIT_C))
 
 
 class CPU
 {
 public:
 
+	static u16 pc;
+	static u16 sp;
+
 	static u16 af;
 	static u16 bc;
 	static u16 de;
 	static u16 hl;
-	static u16 sp;
-	static u16 pc;
 
-	static u8* a;
-	static u8* f;
-	static u8* b;
-	static u8* c;
-	static u8* d;
-	static u8* e;
-	static u8* h;
-	static u8* l;
+	static u8& a;
+	static u8& f;
+	static u8& b;
+	static u8& c;
+	static u8& d;
+	static u8& e;
+	static u8& h;
+	static u8& l;
 
 	static void Initialize();
 	static void Reset();
 
 
 private:
+
+	/*
+	static void AddrImmediate(u8* op1);
+
+	static void AddrImmediateExtended(u16* op1);
+	//static void AddrImmediateExtended(u8* op1, u8* op2);
+	*/
+
 
 	//Can't these be reduced?  Should never need to use address?  Done by addressing part of opcode?
 
@@ -58,7 +88,7 @@ private:
 
 	static void ExecBIT(u8 value, int n);
 
-	static void ExecCALL(bool test, u16 address);
+	static void ExecCALL(bool test, u16 address);	//TODO
 
 	static void ExecCCF();
 
@@ -159,5 +189,49 @@ private:
 	static void ExecXOR(u8* target);
 	//static void ExecXOR(u16 address);
 };
+
+
+/*
+
+ Opcode List
+-===========-
+		
+08		LD   (nn),SP
+
+10		STOP
+
+22		LDI  (HL),A
+2A		LDI  A,(HL)
+
+32		LDD  (HL),A
+3A		LDD  A,(HL)
+
+D3		<unused>
+D9		RETI
+DB		<unused>
+DD		<unused>
+
+E0		LD   (FF00+n),A
+E2		LD   (FF00+C),A
+E3		<unused>
+E4		<unused>
+E8		ADD  SP,dd
+EA		LD   (nn),A
+EB		<unused>
+EC		<unused>
+ED		<unused>
+
+F0		LD   A,(FF00+n)
+F2		LD   A,(FF00+C)
+F4		<unused>
+F8		LD   HL,SP+dd
+FA		LD   A,(nn)
+FC		<unused>
+FD		<unused>
+
+CB3X	SWAP r/(HL)
+
+
+*/
 
 #endif
