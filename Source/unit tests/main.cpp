09fe852c715b61891929de75b3c5f430ceefb8d1
arkhaix <bitbucket@xeisnet.com>
2011-05-07 04:10:32 -1000

    - Added a fixture for CPU tests. - Added two quick-and-easy test cases (InitialValues and RegisterMapping). - Removed the hello world tests (ThingsBreak, ThingsWork) refs #1

--- Source/unit tests/main.cpp
+++ Source/unit tests/main.cpp
@@ -1,15 +1,5 @@
 #include "gtest/gtest.h"
 
-TEST(Things, ThingsBreak)
-{
-	ASSERT_EQ(1, 0);
-}
-
-TEST(Things, ThingsWork)
-{
-	ASSERT_EQ(1, 1);
-}
-
 GTEST_API_ int main(int argc, char **argv) 
 {
 	testing::InitGoogleTest(&argc, argv);
