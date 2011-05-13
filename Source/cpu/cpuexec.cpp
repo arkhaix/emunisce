9a794951c956eac4423c6dd15a012f67c4d1eb56
arkhaix <arkhaix@users.noreply.github.com>
2011-05-13 00:40:08 -1000

    - Implemented opcodes 0xe0 - 0xff.  Untested. - Marked the unimplemented/undocumented opcodes with "//?? undocumented" so I can find them later. - Removed the implemented opcodes from the inline quick-reference lists (large comment blocks). refs #3

--- Source/cpu/cpuexec.cpp
+++ Source/cpu/cpuexec.cpp
@@ -1420,6 +1420,7 @@ int CPU::Execute()
 	break;
 
 	case 0xcb:
+		//Handled by ExecCB.  We should never get here.
 	break;
 
 	case 0xcc:
@@ -1497,6 +1498,7 @@ int CPU::Execute()
 
 	case 0xd3:
 		//D3      OUT  (n),A      -
+		//?? undocumented
 	break;
 
 	case 0xd4:
@@ -1569,6 +1571,7 @@ int CPU::Execute()
 
 	case 0xdb:
 		//DB      IN   A,(n)      -
+		//?? undocumented
 	break;
 
 	case 0xdc:
@@ -1588,6 +1591,7 @@ int CPU::Execute()
 
 	case 0xdd:
 		//DD      <IX>            -
+		//?? undocumented
 	break;
 
 	case 0xde:
@@ -1608,51 +1612,104 @@ int CPU::Execute()
 
 
 	case 0xe0:
+		//E0      RET  PO         LD   (FF00+n),A
+		n = ReadNext8();
+		address = 0xff00 + n;
+		ExecLD(&t, a);
+		memory->Write8(address, t);
 	break;
 
 	case 0xe1:
+		//E1		POP HL
+		ExecPOP(&hl);
+		optime += 10;
 	break;
 
 	case 0xe2:
+		//E2      JP   PO,nn      LD   (FF00+C),A
+		address = 0xff00 + c;
+		ExecLD(&t, a);
+		memory->Write8(address, t);
 	break;
 
 	case 0xe3:
+		//E3      EX   (SP),HL    -
+		//?? undocumented
 	break;
 
 	case 0xe4:
+		//E4      CALL P0,nn      -
+		//?? undocumented
 	break;
 
 	case 0xe5:
+		//E5		PUSH HL
+		ExecPUSH(&hl);
+		optime += 11;
 	break;
 
 	case 0xe6:
+		//E6 n		AND n
+		n = ReadNext8();
+		ExecAND(n);
+		optime += 7;
 	break;
 
 	case 0xe7:
+		//E7		RST 20H
+		ExecRST((u16)0x20);
 	break;
 
 	case 0xe8:
+	{
+		//E8      RET  PE         ADD  SP,dd
+		//?? No idea if this method will handle flags correctly
+		s8 sn = (s8)ReadNext8();
+		s16 operand = sn;
+		ExecADD(&sp, (u16)operand);
+		optime += 14;	//?? guessing based on ADD HL, ss (11) + 3 for memory read
+	}
 	break;
 
 	case 0xe9:
+		//E9		JP (HL)
+		ExecJP(hl);
+		optime += 4;
 	break;
 
 	case 0xea:
+		//EA      JP   PE,nn      LD   (nn),A
+		nn = ReadNext16();
+		ExecLD(&t, a);
+		memory->Write8(nn, t);
+		optime += 13;
 	break;
 
 	case 0xeb:
+		//EB      EX   DE,HL      -
+		//?? undocumented
 	break;
 
 	case 0xec:
+		//EC      CALL PE,nn      -
+		//?? undocumented
 	break;
 
 	case 0xed:
+		//ED      <pref>          -
+		//?? undocumented
 	break;
 
 	case 0xee:
+		//EE n		XOR n
+		n = ReadNext8();
+		ExecXOR(n);
+		optime += 7;
 	break;
 
 	case 0xef:
+		//EF		RST 28H
+		ExecRST((u16)0x28);
 	break;
 
 
@@ -1660,51 +1717,109 @@ int CPU::Execute()
 
 
 	case 0xf0:
+		//F0      RET  P          LD   A,(FF00+n)
+		n = ReadNext8();
+		address = 0xff00 + n;
+		t = memory->Read8(address);
+		ExecLD(&a, t);
 	break;
 
 	case 0xf1:
+		//F1		POP AF
+		ExecPOP(&af);
+		optime += 10;
 	break;
 
 	case 0xf2:
+		//F2      JP   P,nn       LD   A,(FF00+C)
+		address = 0xff00 + c;
+		t = memory->Read8(address);
+		ExecLD(&a, t);
 	break;
 
 	case 0xf3:
+		//F3		DI
+		iff1 = false;
+		iff2 = false;
+		optime += 4;
 	break;
 
 	case 0xf4:
+		//F4      CALL P,nn       -
+		//?? undocumented
 	break;
 
 	case 0xf5:
+		//F5		PUSH AF
+		ExecPUSH(&af);
+		optime += 11;
 	break;
 
 	case 0xf6:
+		//F6 n		OR n
+		n = ReadNext8();
+		ExecOR(n);
+		optime += 7;
 	break;
 
 	case 0xf7:
+		//F7		RST 30H
+		ExecRST((u16)0x30);
 	break;
 
 	case 0xf8:
+	{
+		//F8      RET  M          LD   HL,SP+dd
+		s8 offset = (s8)ReadNext8();
+		address = sp + offset;
+		ExecLD(&hl, address);
+		optime += 13;	//?? wild guess
+	}
 	break;
 
 	case 0xf9:
+		//F9		LD SP,HL
+		ExecLD(&sp, hl);
+		optime += 6;
 	break;
 
 	case 0xfa:
+		//FA      JP   M,nn       LD   A,(nn)
+		nn = ReadNext16();
+		t = memory->Read8(nn);
+		ExecLD(&a, t);
+		optime += 13;
 	break;
 
 	case 0xfb:
+		//FB		EI
+		iff1 = true;
+		iff2 = true;
+		optime += 4;
+		//?? todo: maskable interrupts still disabled during this instruction and the next
 	break;
 
 	case 0xfc:
+		//FC      CALL M,nn       -
+		//?? undocumented
 	break;
 
 	case 0xfd:
+		//FD      <IY>            -
+		//?? undocumented
 	break;
 
 	case 0xfe:
+		//FE n		CP n
+		n = ReadNext8();
+		ExecCP(n);
+		optime += 7;
 	break;
 
 	case 0xff:
+		//FF		RST 38H
+		ExecRST((u16)0x38);
+		optime += 11;
 	break;
 
 
@@ -1727,290 +1842,10 @@ int CPU::ExecuteCB()
 Moved, Removed, and Added Opcodes
 Opcode  Z80             GMB
 ---------------------------------------
-08      EX   AF,AF      LD   (nn),SP
-10      DJNZ PC+dd      STOP
-22      LD   (nn),HL    LDI  (HL),A
-2A      LD   HL,(nn)    LDI  A,(HL)
-32      LD   (nn),A     LDD  (HL),A
-3A      LD   A,(nn)     LDD  A,(HL)
-D3      OUT  (n),A      -
-D9      EXX             RETI
-DB      IN   A,(n)      -
-DD      <IX>            -
-E0      RET  PO         LD   (FF00+n),A
-E2      JP   PO,nn      LD   (FF00+C),A
-E3      EX   (SP),HL    -
-E4      CALL P0,nn      -
-E8      RET  PE         ADD  SP,dd
-EA      JP   PE,nn      LD   (nn),A
-EB      EX   DE,HL      -
-EC      CALL PE,nn      -
-ED      <pref>          -
-F0      RET  P          LD   A,(FF00+n)
-F2      JP   P,nn       LD   A,(FF00+C)
-F4      CALL P,nn       -
-F8      RET  M          LD   HL,SP+dd
-FA      JP   M,nn       LD   A,(nn)
-FC      CALL M,nn       -
-FD      <IY>            -
 CB3X    SLL  r/(HL)     SWAP r/(HL)
 Note: The unused (-) opcodes will lock-up the gameboy CPU when used.
 */
 
-/*
-00		NOP			4	1	1
-01 n n		LD BC,nn		10	3	1
-02		LD (BC),A		7	2	1
-03		INC BC			6	1	1
-04		INC B			4	1	1
-05		DEC B			4	1	1
-06 n		LD B,n			7	2	1
-07		RLCA			4	1	1
-08		EX AF,AF’		4	1	1
-09		ADD HL,BC		11	3	1
-0A		LD A,(BC)		7	2	1
-0B		DEC BC			6	1	1
-0C		INC C			4	1	1
-0D		DEC C			4	1	1
-0E n		LD C,n			7	2	1
-0F		RRCA			4	1	1
-10 e		DJNZ (PC+e)		8/13	2/3	1/1	(met/not met)
-11 n n		LD DE,nn		10	3	1
-12		LD (DE),A		7	2	1
-13		INC DE			6	1	1
-14		INC D			4	1	1
-15		DEC D			4	1	1
-16 n		LD D,n			7	2	1
-17		RLA			4	1	1
-18 e		JR (PC+e)		12	3	1
-19		ADD HL,DE		11	3	1
-1A		LD A,(DE)		7	2	1
-1B		DEC DE			6	1	1
-1C		INC E			4	1	1
-1D		DEC E			4	1	1
-1E n		LD E,n			7	2	1
-1F		RRA			4	1	1
-20 e		JR NZ,(PC+e)		12/7	3/2	1/1	(met/not met)
-21 n n		LD HL,nn		10	3	1
-22 n n		LD (nn),HL		16	5	3
-23		INC HL			6	1	1
-24		INC H			4	1	1
-25		DEC H			4	1	1
-26 n		LD H,n			7	2	1
-27		DAA			4	1	1
-28 e		JR Z,(PC+e)		12/7	3/2	1/1	(met/not met)
-29		ADD HL,HL		11	3	1
-2A n n		LD HL,(nn)		16	5	1
-2B		DEC HL			6	1	1
-2C		INC L			4	1	1
-2D		DEC L			4	1	1
-2E n		LD L,n			7	2	1
-2F		CPL			4	1	1
-30 e		JR NC,(PC+e)		12/7	3/2	1/1	(met/not met)
-31 n n		LD SP,nn		10	3	1
-32 n n		LD (nn),A		13	4	1
-33		INC SP			6	1	1
-34		INC (HL)		11	3	1
-35		DEC (HL)		11	3	1
-36 n		LD (HL),n		10	3	1
-37		SCF			4	1	1
-38 e		JR C,(PC+e)		12/7	3/2	1/1	(met/not met)
-39		ADD HL,SP		11	3	1
-3A n n		LD A,(nn)		13	4	1
-3B		DEC SP			6	1	1
-3C		INC A			4	1	1
-3D		DEC A			4	1	1
-3E n		LD A,n			7	2	1
-3F		CCF			4	1	1
-40		LD B,B			4	1	1
-41		LD B,C			4	1	1
-42		LD B,D			4	1	1
-43		LD B,E			4	1	1
-44		LD B,H			4	1	1
-45		LD B,L			4	1	1
-46		LD B,(HL)		7	2	1
-47		LD B,A			4	1	1
-48		LD C,B			4	1	1
-49		LD C,C			4	1	1
-4A		LD C,D			4	1	1
-4B		LD C,E			4	1	1
-4C		LD C,H			4	1	1
-4D		LD C,L			4	1	1
-4E		LD C,(HL)		7	2	1
-4F		LD C,A			4	1	1
-50		LD D,B			4	1	1
-51		LD D,C			4	1	1
-52		LD D,D			4	1	1
-53		LD D,E			4	1	1
-54		LD D,H			4	1	1
-55		LD D,L			4	1	1
-56		LD D,(HL)		7	2	1
-57		LD D,A			4	1	1
-58		LD E,B			4	1	1
-59		LD E,C			4	1	1
-5A		LD E,D			4	1	1
-5B		LD E,E			4	1	1
-5C		LD E,H			4	1	1
-5D		LD E,L			4	1	1
-5E		LD E,(HL)		7	2	1
-5F		LD E,A			4	1	1
-60		LD H,B			4	1	1
-61		LD H,C			4	1	1
-62		LD H,D			4	1	1
-63		LD H,E			4	1	1
-64		LD H,H			4	1	1
-65		LD H,L			4	1	1
-66		LD H,(HL)		7	2	1
-67		LD H,A			4	1	1
-68		LD L,B			4	1	1
-69		LD L,C			4	1	1
-6A		LD L,D			4	1	1
-6B		LD L,E			4	1	1
-6C		LD L,H			4	1	1
-6D		LD L,L			4	1	1
-6E		LD L,(HL)		7	2	1
-6F		LD L,A			4	1	1
-70		LD (HL),B		7	2	1
-71		LD (HL),C		7	2	1
-72		LD (HL),D		7	2	1
-73		LD (HL),E		7	2	1
-74		LD (HL),H		7	2	1
-75		LD (HL),L		7	2	1
-76		HALT			4	1	1	(repeated till next int)
-77		LD (HL),A		7	2	1
-78		LD A,B			4	1	1
-79		LD A,C			4	1	1
-7A		LD A,D			4	1	1
-7B		LD A,E			4	1	1
-7C		LD A,H			4	1	1
-7D		LD A,L			4	1	1
-7E		LD A,(HL)		7	2	1
-7F		LD A,A			4	1	1
-80		ADD A,B			4	1	1
-81		ADD A,C			4	1	1
-82		ADD A,D			4	1	1
-83		ADD A,E			4	1	1
-84		ADD A,H			4	1	1
-85		ADD A,L			4	1	1
-86		ADD A,(HL)		7	2	1
-87		ADD A,A			4	1	1
-88		ADC A,B			4	1	1
-89		ADC A,C			4	1	1
-8A		ADC A,D			4	1	1
-8B		ADC A,E			4	1	1
-8C		ADC A,H			4	1	1
-8D		ADC A,L			4	1	1
-8E		ADC A,(HL)		7	2	1
-8F		ADC A,A			4	1	1
-90		SUB B			4	1	1
-91		SUB C			4	1	1
-92		SUB D			4	1	1
-93		SUB E			4	1	1
-94		SUB H			4	1	1
-95		SUB L			4	1	1
-96		SUB (HL)		7	2	1
-97		SUB A			4	1	1
-98		SBC A,B			4	1	1
-99		SBC A,C			4	1	1
-9A		SBC A,D			4	1	1
-9B		SBC A,E			4	1	1
-9C		SBC A,H			4	1	1
-9D		SBC A,L			4	1	1
-9E		SBC A,(HL)		7	2	1
-9F		SBC A,A			4	1	1
-A0		AND B			4	1	1
-A1		AND C			4	1	1
-A2		AND D			4	1	1
-A3		AND E			4	1	1
-A4		AND H			4	1	1
-A5		AND L			4	1	1
-A6		AND (HL)		7	2	1
-A7		AND A			4	1	1
-A8		XOR B			4	1	1
-A9		XOR C			4	1	1
-AA		XOR D			4	1	1
-AB		XOR E			4	1	1
-AC		XOR H			4	1	1
-AD		XOR L			4	1	1
-AE		XOR (HL)		7	2	1
-AF		XOR A			4	1	1
-B0		OR B			4	1	1
-B1		OR C			4	1	1
-B2		OR D			4	1	1
-B3		OR E			4	1	1
-B4		OR H			4	1	1
-B5		OR L			4	1	1
-B6		OR (HL)			7	2	1
-B7		OR A			4	1	1
-B8		CP B			4	1	1
-B9		CP C			4	1	1
-BA		CP D			4	1	1
-BB		CP E			4	1	1
-BC		CP H			4	1	1
-BD		CP L			4	1	1
-BE		CP (HL)			7	2	1
-BF		CP A			4	1	1
-C0		RET NZ			11/5	3/1	1/1	(met/not met)
-C1		POP BC			10	3	1
-C2 n n		JP NZ,(nn)		10	3	1	(met or not)
-C3 n n		JP (nn)			10	3	1
-C4 n n		CALL NZ,(nn)		17/10	5/3	1/1	(met/not met)
-C5		PUSH BC			11	3	1
-C6 n		ADD A,n			7	2	1
-C7		RST 0H			11	3	1
-C8		RET Z			11/5	3/1	1/1	(met/not met)
-C9		RET			10	3	1
-CA n n		JP Z,(nn)		10	3	1	(always same)
-CC n n		CALL Z,(nn)		17/10	5/3	1/1	(met/not met)
-CD n n		CALL (nn)		17	5	1
-CE n		ADC A,n			7	2	1
-CF		RST 8H			11	3	1
-D0		RET NC			5	1	1
-D1		POP DE			10	3	1
-D2 n n		JP NC,(nn)		10	3	1	(met or not)
-D3 n		OUT (n),A		11	3	1
-D4 n n		CALL NC,(nn)		17/10	5/3	1/1	(met/not met)
-D5		PUSH DE			11	3	1
-D6 n		SUB n			7	2	1
-D7		RST 10H			11	3	1
-D8		RET C			5	1	1
-D9		EXX			4	1	1
-DA n n		JP C,(nn)		10	3	1	(met or not)
-DB n		IN A,(n)		11	3	1
-DC n n		CALL C,(nn)		17/10	5/3	1
-DE n		SBC A,n
-DF		RST 18H
-E0		RET PO
-E1		POP HL
-E2 n n		JP PO,(nn)
-E3		EX (SP),HL
-E4 n n		CALL PO,(nn)
-E5		PUSH HL
-E6 n		AND n
-E7		RST 20H
-E8		RET PE
-E9		JP (HL)
-EA n n		JP PE,(nn)
-EB		EX DE,HL
-EC n n		CALL PE,(nn)
-EE n		XOR n
-EF		RST 28H
-F0		RET P
-F1		POP AF
-F2 n n		JP P,(nn)
-F3		DI
-F4 n n		CALL P,(nn)
-F5		PUSH AF
-F6 n		OR n
-F7		RST 30H
-F8		RET M
-F9		LD SP,HL
-FA n n		JP M,(nn)
-FB		EI
-FC n n		CALL M,(nn)
-FE n		CP n
-FF		RST 38H
-*/
 
 /*
 CB00		RLC B			8	2	2
