a7b3dcd898cf4f0cc96917d0926288d4c89bb155
arkhaix <bitbucket@xeisnet.com>
2011-05-14 09:16:44 -1000

    - Implemented Memory::CreateFromFile with support from RomOnly.  It also no longer takes a Machine* (obsoleted by SetMachine). - Implemented RomOnly::LoadFile. - RomOnly now determines from the cartridge type whether or not it has cart RAM. - The RAM buffers in Memory and RomOnly are now randomized at startup. - Renamed RomOnly.h/cpp to romonly.h/cpp (all lowercase, made consistent with the rest of the project). refs #18 refs #9

--- Source/memory/romonly.h
+++ Source/memory/romonly.h
@@ -7,6 +7,8 @@ class RomOnly : public Memory
 {
 public:
 
+	RomOnly();
+
 	virtual void Initialize();
 	virtual void Reset();
 
@@ -15,7 +17,9 @@ protected:
 	virtual bool LoadFile(const char* filename);
 
 	u8 m_cartRomData[0x8000];	//0x0000 - 0x7fff = cart rom
-	u8 m_cartRamData[0x2000];	//0xa000 - 0xbfff = cart ram //todo: Not all carts have this, but I don't know how to detect that.
+	u8 m_cartRamData[0x2000];	//0xa000 - 0xbfff = cart ram.
+
+	bool m_hasCartRam;	///<Determined from the cart header.
 };
 
 #endif
