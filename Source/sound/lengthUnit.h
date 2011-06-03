fc268f3d38a2c2db0170fbaf0cb8d03526b9a7ee
arkhaix <bitbucket@xeisnet.com>
2011-06-03 03:22:09 -1000

    - Added a LengthUnit class to the emulated sound component.  This is a first step in refactoring things, but I really needed to add it in order to fix the way length counters work. - Replaced the m_soundXStartTimeSeconds and m_soundXLengthSeconds variables with an instance of the LengthUnit class (one for each sound channel).  Eventually, each sound channel should have its own class, and the length unit will be part of that, but for now it's still an improvement to have it in the Sound class. - The generators are now updated during every Sound::Run call, and not just when a new sample is needed. - Fixed a host of length counter issues that were exposed by Blargg's 02-len ctr test. - The emulator now passes the first two of Blargg's dmg sound tests (01-registers and 02-len ctr), but still fails the 10 remaining tests. refs #39 refs #45 refs #46 refs #47 refs #50

---
+++ Source/sound/lengthUnit.h
@@ -0,0 +1,36 @@
+#ifndef LENGTHUNIT_H
+#define LENGTHUNIT_H
+
+class LengthUnit
+{
+public:
+
+	LengthUnit();
+
+	void SetTicksPerSecond(double ticksPerSecond);
+	void SetDecrementsPerSecond(double decrementsPerSecond);
+
+	void SetMaxLength(int maxLength);
+	void SetCurrentLength(int currentLength);
+	void SetInverseLength(int inverseLength);
+
+	int GetCurrentLength();
+
+	void Run(int ticks);
+
+
+private:
+
+	void UpdateTicksPerDecrement();
+
+	double m_ticksPerSecond;
+	double m_decrementsPerSecond;
+
+	double m_ticksPerDecrement;
+	double m_ticksUntilNextDecrement;
+
+	int m_maxLength;
+	int m_currentLength;
+};
+
+#endif
