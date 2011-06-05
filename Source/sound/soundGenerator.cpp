9744fd6a9f62df144ee8df36ad0338e4897ecb12
arkhaix <bitbucket@xeisnet.com>
2011-06-05 05:29:59 -1000

    - Added SoundGenerator base class for Sound1-4.  I'm planning on putting length and envelope stuff in there (sound3 can just override and disable the envelope). refs #46

---
+++ Source/sound/soundGenerator.cpp
@@ -0,0 +1,43 @@
+#include "soundGenerator.h"
+
+
+SoundGenerator::SoundGenerator()
+{
+	m_hasPower = true;
+}
+
+
+void SoundGenerator::Initialize()
+{
+}
+
+
+void SoundGenerator::PowerOff()
+{
+	m_hasPower = false;
+}
+
+void SoundGenerator::PowerOn()
+{
+	m_hasPower = true;
+}
+
+
+void SoundGenerator::Run(int ticks)
+{
+}
+
+
+void SoundGenerator::TickLength()
+{
+}
+
+void SoundGenerator::TickEnvelope()
+{
+}
+
+
+float SoundGenerator::GetSample()
+{
+	return 0.f;
+}
