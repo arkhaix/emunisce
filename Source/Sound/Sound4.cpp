a7f3ed89f8538308b612d7adf1fdc9f0377ecc05
arkhaix <arkhaix@arkhaix.com>
2011-06-08 19:11:01 -1000

    - Added the GPLv2 license as License.txt. - Added the copyright/license header to all source files. refs #58

--- Source/Sound/Sound4.cpp
+++ Source/Sound/Sound4.cpp
@@ -1,3 +1,22 @@
+/*
+Copyright (C) 2011 by Andrew Gray
+arkhaix@arkhaix.com
+
+This file is part of PhoenixGB.
+
+PhoenixGB is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+version 2 as published by the Free Software Foundation.
+The full license is available at http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+PhoenixGB is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with PhoenixGB.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include "Sound4.h"
 
 #include "../Common/Machine.h"
