e7404368f42a55239e5ae581d6ff6430f1e50322
arkhaix <arkhaix@arkhaix.com>
2011-06-16 11:44:06 -1000

    - Added the foundation for a simple Serialization library under Utility.  It contains an ISerializer interface for adding output targets, and an Archive class which defines a common entry point for serializing the basic platform types. refs #85

---
+++ Source/Utility/Serialization/Archive.cpp
@@ -0,0 +1,133 @@
+/*
+Copyright (C) 2011 by Andrew Gray
+arkhaix@arkhaix.com
+
+This file is part of PhoenixGB.
+
+PhoenixGB is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+version 2 as published by the Free Software Foundation.
+The full license is available at http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+PhoenixGB is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with PhoenixGB.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "Archive.h"
+
+#include "ISerializer.h"
+
+
+Archive::Archive(ISerializer* serializer, ArchiveMode::Type archiveMode)
+{
+	m_serializer = serializer;
+	m_archiveMode = archiveMode;
+}
+
+
+Archive& Archive::operator&(u8& data)
+{
+	if(m_archiveMode == ArchiveMode::Saving)
+		m_serializer->Save((unsigned char*)&data, sizeof(data));
+	else
+		m_serializer->Restore((unsigned char*)&data, sizeof(data));
+
+	return *this;
+}
+
+Archive& Archive::operator&(u16& data)
+{
+	if(m_archiveMode == ArchiveMode::Saving)
+		m_serializer->Save((unsigned char*)&data, sizeof(data));
+	else
+		m_serializer->Restore((unsigned char*)&data, sizeof(data));
+
+	return *this;
+}
+
+Archive& Archive::operator&(u32& data)
+{
+	if(m_archiveMode == ArchiveMode::Saving)
+		m_serializer->Save((unsigned char*)&data, sizeof(data));
+	else
+		m_serializer->Restore((unsigned char*)&data, sizeof(data));
+
+	return *this;
+}
+
+Archive& Archive::operator&(u64& data)
+{
+	if(m_archiveMode == ArchiveMode::Saving)
+		m_serializer->Save((unsigned char*)&data, sizeof(data));
+	else
+		m_serializer->Restore((unsigned char*)&data, sizeof(data));
+
+	return *this;
+}
+
+
+Archive& Archive::operator&(s8& data)
+{
+	if(m_archiveMode == ArchiveMode::Saving)
+		m_serializer->Save((unsigned char*)&data, sizeof(data));
+	else
+		m_serializer->Restore((unsigned char*)&data, sizeof(data));
+
+	return *this;
+}
+
+Archive& Archive::operator&(s16& data)
+{
+	if(m_archiveMode == ArchiveMode::Saving)
+		m_serializer->Save((unsigned char*)&data, sizeof(data));
+	else
+		m_serializer->Restore((unsigned char*)&data, sizeof(data));
+
+	return *this;
+}
+
+Archive& Archive::operator&(s32& data)
+{
+	if(m_archiveMode == ArchiveMode::Saving)
+		m_serializer->Save((unsigned char*)&data, sizeof(data));
+	else
+		m_serializer->Restore((unsigned char*)&data, sizeof(data));
+
+	return *this;
+}
+
+Archive& Archive::operator&(s64& data)
+{
+	if(m_archiveMode == ArchiveMode::Saving)
+		m_serializer->Save((unsigned char*)&data, sizeof(data));
+	else
+		m_serializer->Restore((unsigned char*)&data, sizeof(data));
+
+	return *this;
+}
+
+
+Archive& Archive::operator&(float& data)
+{
+	if(m_archiveMode == ArchiveMode::Saving)
+		m_serializer->Save((unsigned char*)&data, sizeof(data));
+	else
+		m_serializer->Restore((unsigned char*)&data, sizeof(data));
+
+	return *this;
+}
+
+Archive& Archive::operator&(double& data)
+{
+	if(m_archiveMode == ArchiveMode::Saving)
+		m_serializer->Save((unsigned char*)&data, sizeof(data));
+	else
+		m_serializer->Restore((unsigned char*)&data, sizeof(data));
+
+	return *this;
+}
+
