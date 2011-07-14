1f3790a87890b57cf5e823fce46ec724a9dfb10c
arkhaix <arkhaix@users.noreply.github.com>
2011-07-14 08:40:34 -0800

    - Added stubs for LinuxPlatform's Event and Thread classes. - Removed LinuxPlatform's Window and Window_Internal classes.  Windows have been declared application-specific, not platform-specific. - Updated the Code::Blocks projects as necessary to get everything building under Linux.  Haven't tried a Windows build yet. refs #98

---
+++ Source/LinuxPlatform/Event.h
@@ -0,0 +1,47 @@
+/*
+Copyright (C) 2011 by Andrew Gray
+arkhaix@emunisce.com
+
+This file is part of Emunisce.
+
+Emunisce is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+version 2 as published by the Free Software Foundation.
+The full license is available at http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+Emunisce is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Emunisce.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef EVENT_H
+#define EVENT_H
+
+namespace Emunisce
+{
+
+class Event
+{
+public:
+
+	Event(bool autoReset = false);
+	~Event();
+
+	void Set();		///<Sets state to signaled, allowing waiting threads to continue
+	void Reset();	///<Sets state to non-signaled, causing threads to wait
+
+	void Wait();
+
+private:
+
+/*
+	HANDLE m_event;
+*/
+};
+
+}   //namespace Emunisce
+
+#endif
---
+++ Source/LinuxPlatform/Thread.cpp
@@ -0,0 +1,65 @@
+/*
+Copyright (C) 2011 by Andrew Gray
+arkhaix@emunisce.com
+
+This file is part of Emunisce.
+
+Emunisce is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+version 2 as published by the Free Software Foundation.
+The full license is available at http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+Emunisce is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Emunisce.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "Thread.h"
+using namespace Emunisce;
+
+
+Thread::Thread()
+{
+
+}
+
+Thread::~Thread()
+{
+
+}
+
+
+void Thread::Start(void* param)
+{
+
+}
+
+void Thread::Stop()
+{
+
+}
+
+void Thread::Join(unsigned int timeoutMilliseconds)
+{
+
+}
+
+
+bool Thread::IsRunning()
+{
+    return false;
+}
+
+
+int Thread::GetCurrentThreadId()
+{
+    return 0;
+}
+
+int Thread::GetThreadId()
+{
+    return 0;
+}
