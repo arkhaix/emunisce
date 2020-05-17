arkhaix/emunisce

commit 63a9964b286442816c7e605df484ab4368589ecd

Added braces to all conditionals and ran visual studio autoformat on everything
- clang-tidy-all.ps1 is the clang-tidy script I ran to fix all the braces.
- Ran the VS extension "Format All Files" which just does visual studio's default formatting on every file in the solution.  Most importantly, this fixed all the spots with mixed tabs and spaces.  This also cleaned up any weird brace placement that the clang-tidy step left behind.
- refs #2

Utility/Serialization/MemorySerializer.cpp
@@ -20,7 +20,7 @@ along with Emunisce.  If not, see <http://www.gnu.org/licenses/>.
 #include "MemorySerializer.h"
 using namespace Emunisce;
 
-#include <memory.h>
+#include <Memory.h>
 #include <stdlib.h>
 
 
@@ -37,8 +37,9 @@ MemorySerializer::MemorySerializer()
 
 MemorySerializer::~MemorySerializer()
 {
-	if(m_buffer != nullptr)
+	if (m_buffer != nullptr) {
 		free(m_buffer);
+	}
 }
 
 
@@ -54,8 +55,9 @@ unsigned int MemorySerializer::GetBufferSize()
 
 void MemorySerializer::TransferBuffer(unsigned char** buffer, unsigned int* size)
 {
-	if(buffer == nullptr || size == nullptr)
+	if (buffer == nullptr || size == nullptr) {
 		return;
+	}
 
 	*buffer = m_buffer;
 	*size = m_usedSize;
@@ -87,7 +89,7 @@ void MemorySerializer::SetArchive(Archive* archive)
 
 void MemorySerializer::Save(unsigned char* data, unsigned int bytes)
 {
-	if(m_usedSize + bytes >= m_reservedSize)
+	if (m_usedSize + bytes >= m_reservedSize)
 	{
 		float fNewSize = (float)(m_reservedSize + bytes) * m_reserveMultiplier;
 		unsigned int newSize = (unsigned int)fNewSize;
@@ -96,21 +98,23 @@ void MemorySerializer::Save(unsigned char* data, unsigned int bytes)
 
 		memcpy((void*)newBuffer, m_buffer, m_usedSize);
 
-		if(m_buffer != nullptr)
+		if (m_buffer != nullptr) {
 			free(m_buffer);
+		}
 
 		m_buffer = newBuffer;
 		m_reservedSize = newSize;
 	}
 
-	memcpy((void*)(m_buffer+ m_usedSize), (void*)data, bytes);
+	memcpy((void*)(m_buffer + m_usedSize), (void*)data, bytes);
 	m_usedSize += bytes;
 }
 
 void MemorySerializer::Restore(unsigned char* buffer, unsigned int bytes)
 {
-	if(m_buffer == nullptr || buffer == nullptr)
+	if (m_buffer == nullptr || buffer == nullptr) {
 		return;
+	}
 
 	memcpy((void*)buffer, (void*)(m_buffer + m_usedSize), bytes);
 	m_usedSize += bytes;

commit 7cf469c53c5665d39c59831547561258a7902a18

Fixed include in MemorySerializer.cpp
- clang-tidy broke this in 63a9964
- refs #2

Utility/Serialization/MemorySerializer.cpp
@@ -20,7 +20,7 @@ along with Emunisce.  If not, see <http://www.gnu.org/licenses/>.
 #include "MemorySerializer.h"
 using namespace Emunisce;
 
-#include <Memory.h>
+#include <memory.h>
 #include <stdlib.h>
 
 // MemorySerializer